887acb0386c5ed8d2ae59273006e140b7345c079
Denis Ferreira <denisferreira93@hotmail.com>
2022-04-28 16:36:20 -0300

    1.2.3 - Removendo aluno da lista - Aula 3: Removendo aluno por completo

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -25,6 +25,8 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     public static final String TITULO_APPBAR = "Lista de alunos";
     private final AlunoDAO dao = new AlunoDAO();
+    private ArrayAdapter<Aluno> adapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +67,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int posicao, long id) {
                Aluno alunoEscolhido = (Aluno) adapterView.getItemAtPosition(posicao);
                dao.remove(alunoEscolhido);
+               adapter.remove(alunoEscolhido);
                return true;
            }
        });
@@ -87,7 +90,8 @@ public class ListaAlunosActivity extends AppCompatActivity {
     }
 
     private void configuraAdapter(ListView listaDeAlunos, List<Aluno> alunos) {
-        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
+        listaDeAlunos.setAdapter(adapter);
     }
 }
 
